e587f9b1795ffed17fb4af99da050f96cbf1fa2e
Ozymo Alt <nvhai.contact@gmail.com>
2024-08-02 02:22:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyautogui
 import ctypes
-from ctypes import windll, wintypes
+import ctypes.wintypes as wintypes
 import os
 from colorbot import Colorbot
 from settings import Settings
@@ -18,14 +18,23 @@ class Main:
         hwnd = ctypes.windll.kernel32.GetConsoleWindow()
         if hwnd:
             style = ctypes.windll.user32.GetWindowLongW(hwnd, -16)
-            style &= ~0x00040000  # WS_MAXIMIZEBOX
-            style &= ~0x00020000  # WS_MINIMIZEBOX
+            style &= ~0x00080000  # Remove WS_MAXIMIZEBOX
+            style &= ~0x00C00000  # Remove WS_SIZEBOX
             ctypes.windll.user32.SetWindowLongW(hwnd, -16, style)
-        STD_OUTPUT_HANDLE_ID = ctypes.c_ulong(4294967285)
+        
+        STD_OUTPUT_HANDLE_ID = -11
+        windll = ctypes.windll.kernel32
         handle = windll.GetStdHandle(STD_OUTPUT_HANDLE_ID)
-        rect = ctypes.wintypes.SMALL_RECT(0, 0, width - 1, height - 1)
-        windll.SetConsoleScreenBufferSize(handle, ctypes.wintypes._COORD(width, height))
-        windll.SetConsoleWindowInfo(handle, ctypes.c_int(True), ctypes.pointer(rect))
+        
+        if not handle:
+            raise Exception("Failed to get standard output handle")
+
+        # Set console buffer size
+        windll.SetConsoleScreenBufferSize(handle, wintypes._COORD(width, height))
+
+        # Set console window size
+        rect = wintypes.SMALL_RECT(0, 0, width - 1, height - 1)
+        windll.SetConsoleWindowInfo(handle, True, ctypes.byref(rect))
 
     def info(self):
         os.system('cls')

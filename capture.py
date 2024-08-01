084c49d9831f2e0e1033380171ae96d98fa64220
Ozymo Alt <nvhai.contact@gmail.com>
2024-08-02 02:10:26 +0700

    Update capture.py

--- capture.py
+++ capture.py
@@ -1,13 +1,43 @@
 import numpy as np
 from mss import mss
+import pyautogui
 
 class Capture:
-    def __init__(self, x, y, xfov, yfov):
+    def __init__(self, x, y, xfov, yfov, region=None):
         self.mss = mss()
-        self.monitor = {'top': y, 'left': x, 'width': xfov, 'height': yfov}
         self.xfov = xfov
         self.yfov = yfov
+        
+        if region:
+            # Region is a tuple (x, y, width, height)
+            self.monitor = {
+                'top': region[1],
+                'left': region[0],
+                'width': region[2],
+                'height': region[3]
+            }
+        else:
+            # Use full screen dimensions
+            self.monitor = {
+                'top': y,
+                'left': x,
+                'width': xfov,
+                'height': yfov
+            }
 
     def get_screen(self):
+        # Capture the screen or the specified region
         screenshot = self.mss.grab(self.monitor)
         return np.array(screenshot)
+
+    def update_region(self, region):
+        # Update the region dynamically
+        self.monitor = {
+            'top': region[1],
+            'left': region[0],
+            'width': region[2],
+            'height': region[3]
+        }
+
+def get_screen_resolution():
+    return pyautogui.size()

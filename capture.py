b5e4d8f06089bd4f71ed9c8f7481f39282b6a587
Ozymo Alt <nvhai.contact@gmail.com>
2024-08-02 11:02:58 +0700

    Update capture.py

--- capture.py
+++ capture.py
@@ -3,6 +3,7 @@ import bettercam
 from screeninfo import get_monitors
 import threading
 import queue
+import logging
 from settings import Settings
 
 class Capture(threading.Thread):
@@ -11,53 +12,65 @@ class Capture(threading.Thread):
         self.daemon = True
         self.name = "Capture"
         self.settings = Settings()
-        
+
         self.print_startup_messages()
-        
+
         self._custom_region = []
         self._offset_x = None
         self._offset_y = None
-        
-        self.screen_x_center = int(self.settings.get_int('Capture', 'detection_window_width') / 2)
-        self.screen_y_center = int(self.settings.get_int('Capture', 'detection_window_height') / 2)
+
+        self.screen_x_center = self.settings.get_int('Capture', 'detection_window_width') // 2
+        self.screen_y_center = self.settings.get_int('Capture', 'detection_window_height') // 2
 
         self.prev_detection_window_width = self.settings.get_int('Capture', 'detection_window_width')
         self.prev_detection_window_height = self.settings.get_int('Capture', 'detection_window_height')
         self.prev_bettercam_capture_fps = self.settings.get_int('Capture', 'bettercam_capture_fps')
-        
+
         self.frame_queue = queue.Queue(maxsize=1)
         self.running = True
-        
+
         if self.settings.get_boolean('Capture', 'Bettercam_capture'):
             self.setup_bettercam()
         elif self.settings.get_boolean('Capture', 'Obs_capture'):
             self.setup_obs()
-            
+    
     def setup_bettercam(self):
-        self.bc = bettercam.create(device_idx=self.settings.get_int('Capture', 'bettercam_monitor_id'),
-                                   output_idx=self.settings.get_int('Capture', 'bettercam_gpu_id'),
-                                   output_color="BGR",
-                                   max_buffer_len=16,
-                                   region=self.Calculate_screen_offset())
-        if not self.bc.is_capturing:
-            self.bc.start(region=self.Calculate_screen_offset(custom_region=[] if len(self._custom_region) <=0 else self._custom_region,
-                                                              x_offset=None if self._offset_x == None else self._offset_x,
-                                                              y_offset = None if self._offset_y == None else self._offset_y),
-                          target_fps=self.settings.get_int('Capture', 'bettercam_capture_fps'))
+        try:
+            self.bc = bettercam.create(
+                device_idx=self.settings.get_int('Capture', 'bettercam_monitor_id'),
+                output_idx=self.settings.get_int('Capture', 'bettercam_gpu_id'),
+                output_color="BGR",
+                max_buffer_len=16,
+                region=self.calculate_screen_offset()
+            )
+            if not self.bc.is_capturing:
+                self.bc.start(
+                    region=self.calculate_screen_offset(
+                        custom_region=[] if len(self._custom_region) == 0 else self._custom_region,
+                        x_offset=self._offset_x,
+                        y_offset=self._offset_y
+                    ),
+                    target_fps=self.settings.get_int('Capture', 'bettercam_capture_fps')
+                )
+        except Exception as e:
+            logging.error(f"Failed to setup Bettercam: {e}")
 
     def setup_obs(self):
-        if self.settings.get('Capture', 'Obs_camera_id') == 'auto':
-            camera_id = self.find_obs_virtual_camera()
-            if camera_id == -1:
-                print('OBS Virtual Camera not found')
-                exit(0)
-        elif self.settings.get('Capture', 'Obs_camera_id').isdigit():
-            camera_id = int(self.settings.get('Capture', 'Obs_camera_id'))
-        self.obs_camera = cv2.VideoCapture(camera_id)
-        self.obs_camera.set(cv2.CAP_PROP_FRAME_WIDTH, self.settings.get_int('Capture', 'detection_window_width'))
-        self.obs_camera.set(cv2.CAP_PROP_FRAME_HEIGHT, self.settings.get_int('Capture', 'detection_window_height'))
-        self.obs_camera.set(cv2.CAP_PROP_FPS, self.settings.get_int('Capture', 'Obs_capture_fps'))
-        
+        try:
+            if self.settings.get('Capture', 'Obs_camera_id') == 'auto':
+                camera_id = self.find_obs_virtual_camera()
+                if camera_id == -1:
+                    logging.error('OBS Virtual Camera not found')
+                    exit(0)
+            elif self.settings.get('Capture', 'Obs_camera_id').isdigit():
+                camera_id = int(self.settings.get('Capture', 'Obs_camera_id'))
+            self.obs_camera = cv2.VideoCapture(camera_id)
+            self.obs_camera.set(cv2.CAP_PROP_FRAME_WIDTH, self.settings.get_int('Capture', 'detection_window_width'))
+            self.obs_camera.set(cv2.CAP_PROP_FRAME_HEIGHT, self.settings.get_int('Capture', 'detection_window_height'))
+            self.obs_camera.set(cv2.CAP_PROP_FPS, self.settings.get_int('Capture', 'Obs_capture_fps'))
+        except Exception as e:
+            logging.error(f"Failed to setup OBS: {e}")
+
     def run(self):
         while self.running:
             frame = self.capture_frame()
@@ -65,7 +78,7 @@ class Capture(threading.Thread):
                 if self.frame_queue.full():
                     self.frame_queue.get()
                 self.frame_queue.put(frame)
-            
+
     def capture_frame(self):
         if self.settings.get_boolean('Capture', 'Bettercam_capture'):
             return self.bc.get_latest_frame()
@@ -74,72 +87,67 @@ class Capture(threading.Thread):
             if ret_val:
                 return img
             else:
-                print('Failed to capture frame from OBS Virtual Camera')
+                logging.error('Failed to capture frame from OBS Virtual Camera')
                 return None
-            
+
     def get_new_frame(self):
         try:
             return self.frame_queue.get(timeout=1)
         except queue.Empty:
             return None
-    
+
     def restart(self):
         if self.settings.get_boolean('Capture', 'Bettercam_capture') and (
-                self.prev_detection_window_height != self.settings.get_int('Capture', 'detection_window_height') or 
-                self.prev_detection_window_width != self.settings.get_int('Capture', 'detection_window_width') or 
+                self.prev_detection_window_height != self.settings.get_int('Capture', 'detection_window_height') or
+                self.prev_detection_window_width != self.settings.get_int('Capture', 'detection_window_width') or
                 self.prev_bettercam_capture_fps != self.settings.get_int('Capture', 'bettercam_capture_fps')):
             self.bc.stop()
             del self.bc
             self.setup_bettercam()
 
-            self.screen_x_center = self.settings.get_int('Capture', 'detection_window_width') / 2
-            self.screen_y_center = self.settings.get_int('Capture', 'detection_window_height') / 2
+            self.screen_x_center = self.settings.get_int('Capture', 'detection_window_width') // 2
+            self.screen_y_center = self.settings.get_int('Capture', 'detection_window_height') // 2
 
             self.prev_detection_window_width = self.settings.get_int('Capture', 'detection_window_width')
             self.prev_detection_window_height = self.settings.get_int('Capture', 'detection_window_height')
 
-            print('Capture reloaded')
-            
-    def Calculate_screen_offset(self, custom_region = [], x_offset=None, y_offset=None):
-        if x_offset is None:
-            x_offset = 0
-        if y_offset is None:
-            y_offset = 0
-        
-        if len(custom_region) <= 0:
+            logging.info('Capture reloaded')
+
+    def calculate_screen_offset(self, custom_region=[], x_offset=0, y_offset=0):
+        if len(custom_region) == 0:
             left, top = self.get_primary_display_resolution()
         else:
             left, top = custom_region
-        
-        left = left / 2 - self.settings.get_int('Capture', 'detection_window_width') / 2 + x_offset
-        top = top / 2 - self.settings.get_int('Capture', 'detection_window_height') / 2 - y_offset
+
+        left = left // 2 - self.settings.get_int('Capture', 'detection_window_width') // 2 + x_offset
+        top = top // 2 - self.settings.get_int('Capture', 'detection_window_height') // 2 - y_offset
         width = left + self.settings.get_int('Capture', 'detection_window_width')
         height = top + self.settings.get_int('Capture', 'detection_window_height')
-        
-        return (int(left), int(top), int(width), int(height))
-    
+
+        return int(left), int(top), int(width), int(height)
+
     def get_primary_display_resolution(self):
         monitors = get_monitors()
         for monitor in monitors:
             if monitor.is_primary:
                 return monitor.width, monitor.height
-            
+
     def find_obs_virtual_camera(self):
         max_tested = 20
         obs_camera_name = 'DSHOW'
-        
+
         for i in range(max_tested):
             cap = cv2.VideoCapture(i, cv2.CAP_DSHOW)
             if not cap.isOpened():
                 continue
             backend_name = cap.getBackendName()
             if backend_name == obs_camera_name:
-                print(f'OBS Virtual Camera found at index {i}')
+                logging.info(f'OBS Virtual Camera found at index {i}')
                 cap.release()
                 return i
             cap.release()
         return -1
-    
+
     def print_startup_messages(self):
         version = 0
         try:
@@ -147,22 +155,22 @@ class Capture(threading.Thread):
                 lines = f.read().split('\n')
                 version = lines[0].split('=')[1]
         except:
-            print('(version file is not found)')
+            logging.warning('Version file not found')
 
-        print(f'(Version {version})\n\n',
-              'Hotkeys:\n',
-              f'[{self.settings.get("Hotkeys", "hotkey_targeting")}] - Aiming at the target\n',
-              f'[{self.settings.get("Hotkeys", "hotkey_exit")}] - EXIT\n',
-              f'[{self.settings.get("Hotkeys", "hotkey_pause")}] - PAUSE AIM\n',
-              f'[{self.settings.get("Hotkeys", "hotkey_reload_config")}] - Reload config\n')
-            
-    def Quit(self):
+        logging.info(f'(Version {version})')
+        logging.info('Hotkeys:')
+        logging.info(f'[{self.settings.get("Hotkeys", "hotkey_targeting")}] - Aiming at the target')
+        logging.info(f'[{self.settings.get("Hotkeys", "hotkey_exit")}] - EXIT')
+        logging.info(f'[{self.settings.get("Hotkeys", "hotkey_pause")}] - PAUSE AIM')
+        logging.info(f'[{self.settings.get("Hotkeys", "hotkey_reload_config")}] - Reload config')
+
+    def quit(self):
         self.running = False
         if self.settings.get_boolean('Capture', 'Bettercam_capture') and self.bc.is_capturing:
             self.bc.stop()
         if self.settings.get_boolean('Capture', 'Obs_capture'):
             self.obs_camera.release()
         self.join()
-            
+
 capture = Capture()
 capture.start()
